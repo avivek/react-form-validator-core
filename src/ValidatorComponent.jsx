e8e6dc0e9146b7844fb5e849ba7b1795c20729f4
vivek anand y <y.vivekanand@gmail.com>
2019-02-21 00:16:29 +0530

    initial commit

---
+++ src/ValidatorComponent.jsx
@@ -0,0 +1,185 @@
+/* eslint-disable */
+import React from 'react';
+import PropTypes from 'prop-types';
+import Promise from 'promise-polyfill';
+/* eslint-enable */
+import { polyfill } from 'react-lifecycles-compat';
+import ValidatorForm from './ValidatorForm';
+import { debounce } from './utils';
+
+class ValidatorComponent extends React.Component {
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.validators && nextProps.errorMessages &&
+            (
+                prevState.validators !== nextProps.validators ||
+                prevState.errorMessages !== nextProps.errorMessages
+            )
+        ) {
+            return {
+                value: nextProps.value,
+                validators: nextProps.validators,
+                errorMessages: nextProps.errorMessages,
+            };
+        }
+        return {
+            value: nextProps.value,
+        };
+    }
+
+    componentOnChange = (event) =>
+    {
+        console.log('the base on Change has been called now');
+        if(event)
+        {
+            event.preventDefault();
+        }
+        this.setChanged(true);
+        if(this.props.onChange)
+        {
+            this.props.onChange(event);
+        }
+    }
+
+    componentOnBlur = (event)=>
+    {
+        console.log('the base on blur has been called now');
+        if(event)
+        {
+            event.preventDefault();
+        }
+        this.setBlurred(true);
+        if(this.props.onBlur)
+        {
+            this.props.onBlur(event);
+        }
+    }
+
+    state = {
+        isValid: true,
+        value: this.props.value,
+        errorMessages: this.props.errorMessages,
+        validators: this.props.validators,
+        changed:false,
+        blurred:false
+    }
+
+    componentDidMount() {
+        this.configure();
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return this.state !== nextState || this.props !== nextProps;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.instantValidate && this.props.value !== prevState.value) {
+            this.validateDebounced(this.props.value, this.props.withRequiredValidator);
+        }
+    }
+
+    componentWillUnmount() {
+        this.context.form.detachFromForm(this);
+        this.validateDebounced.cancel();
+    }
+
+    getErrorMessage = () => {
+        const { errorMessages } = this.state;
+        const type = typeof errorMessages;
+
+        if (type === 'string') {
+            return errorMessages;
+        } else if (type === 'object') {
+            if (this.invalid.length > 0) {
+                return errorMessages[this.invalid[0]];
+            }
+        }
+        // eslint-disable-next-line
+        console.log('unknown errorMessages type', errorMessages);
+        return true;
+    }
+
+    instantValidate = true
+    invalid = []
+
+    configure = () => {
+        this.context.form.attachToForm(this);
+        this.instantValidate = this.context.form.instantValidate;
+        this.debounceTime = this.context.form.debounceTime;
+        this.validateDebounced = debounce(this.validate, this.debounceTime);
+    }
+
+    validate = (value, includeRequired = false, dryRun = false) => {
+        const validations = Promise.all(
+            this.state.validators.map(validator => ValidatorForm.getValidator(validator, value, includeRequired)),
+        );
+
+        validations.then((results) => {
+            this.invalid = [];
+            let valid = true;
+            results.forEach((result, key) => {
+                if (!result) {
+                    valid = false;
+                    this.invalid.push(key);
+                }
+            });
+            if (!dryRun) {
+                this.setState({ isValid: valid}, () => {
+                    this.props.validatorListener(this.state.isValid);
+                });
+            }
+        });
+    }
+
+    isValid = () => this.state.isValid;
+
+    setChanged = (changed) => {
+        this.setState({changed:changed})
+    }
+
+    isChanged = () => {
+        this.state.changed;
+    }
+
+    setBlurred= (blurred) => {
+        this.setState({blurred:blurred})
+    }
+
+    isBlurred = () => {
+        this.state.blurred;
+    }
+
+
+    makeInvalid = () => {
+        this.setState({ isValid: false });
+    }
+
+    makeValid = () => {
+        this.setState({ isValid: true });
+    }
+}
+
+ValidatorComponent.contextTypes = {
+    form: PropTypes.object,
+};
+
+ValidatorComponent.propTypes = {
+    errorMessages: PropTypes.oneOfType([
+        PropTypes.array,
+        PropTypes.string,
+    ]),
+    validators: PropTypes.array,
+    value: PropTypes.any,
+    validatorListener: PropTypes.func,
+    withRequiredValidator: PropTypes.bool,
+};
+
+ValidatorComponent.defaultProps = {
+    errorMessages: 'error',
+    validators: [],
+    validatorListener: () => {},
+};
+
+polyfill(ValidatorComponent);
+
+export default ValidatorComponent;
---
+++ src/ValidatorForm.jsx
@@ -0,0 +1,165 @@
+/* eslint-disable */
+import React from 'react';
+import PropTypes from 'prop-types';
+/* eslint-enable */
+import Rules from './ValidationRules';
+
+class ValidatorForm extends React.Component {
+
+    static getValidator = (validator, value, includeRequired) => {
+        let result = true;
+        let name = validator;
+        if (name !== 'required' || includeRequired) {
+            let extra;
+            const splitIdx = validator.indexOf(':');
+            if (splitIdx !== -1) {
+                name = validator.substring(0, splitIdx);
+                extra = validator.substring(splitIdx + 1);
+            }
+            result = Rules[name](value, extra);
+        }
+        return result;
+    }
+
+    getChildContext = () => ({
+        form: {
+            attachToForm: this.attachToForm,
+            detachFromForm: this.detachFromForm,
+            instantValidate: this.instantValidate,
+            debounceTime: this.debounceTime,
+        },
+    })
+
+    instantValidate = this.props.instantValidate !== undefined ? this.props.instantValidate : true
+    debounceTime = this.props.debounceTime
+    childs = []
+    errors = []
+
+    attachToForm = (component) => {
+        if (this.childs.indexOf(component) === -1) {
+            this.childs.push(component);
+        }
+    }
+
+    detachFromForm = (component) => {
+        const componentPos = this.childs.indexOf(component);
+        if (componentPos !== -1) {
+            this.childs = this.childs.slice(0, componentPos)
+                .concat(this.childs.slice(componentPos + 1));
+        }
+    }
+
+    submit = (event) => {
+        if (event) {
+            event.preventDefault();
+        }
+        this.errors = [];
+        const result = this.walk(this.childs);
+        if (this.errors.length) {
+            this.props.onError(this.errors);
+        }
+        if (result) {
+            this.props.onSubmit(event);
+        }
+        return result;
+    }
+
+    walk = (children, dryRun) => {
+        const self = this;
+        let result = true;
+        if (Array.isArray(children)) {
+            children.forEach((input) => {
+                if (!self.checkInput(input, dryRun)) {
+                    result = false;
+                }
+                return input;
+            });
+        } else {
+            result = self.walk([children], dryRun);
+        }
+        return result;
+    }
+
+    checkInput = (input, dryRun) => {
+        let result = true;
+        const validators = input.props.validators;
+        if (validators && !this.validate(input, true, dryRun)) {
+            result = false;
+        }
+        return result;
+    }
+
+    validate = (input, includeRequired, dryRun) => {
+        const { value, validators } = input.props;
+        input.setState({changed:true,blurred:true});
+        const result = [];
+        let valid = true;
+        let validateResult = false;
+        validators.map((validator) => {
+            validateResult = this.constructor.getValidator(validator, value, includeRequired);
+            result.push({ input, result: validateResult });
+            input.validate(input.props.value, true, dryRun);
+            return validator;
+        });
+        result.map((item) => {
+            if (!item.result) {
+                valid = false;
+                this.errors.push(item.input);
+            }
+            return item;
+        });
+        return valid;
+    }
+
+    find = (collection, fn) => {
+        for (let i = 0, l = collection.length; i < l; i++) {
+            const item = collection[i];
+            if (fn(item)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    resetValidations = () => {
+        this.childs.forEach((child) => {
+            child.validateDebounced.cancel();
+            child.setState({ isValid: true,changed:false,blurred:false });
+        });
+    }
+
+    isFormValid = (dryRun = true) => this.walk(this.childs, dryRun);
+
+    render() {
+        // eslint-disable-next-line
+        const { onSubmit, instantValidate, onError, debounceTime, children, ...rest } = this.props;
+        return (
+            <form {...rest} onSubmit={this.submit}>
+                {children}
+            </form>
+        );
+    }
+}
+
+ValidatorForm.addValidationRule = (name, callback) => {
+    Rules[name] = callback;
+};
+
+ValidatorForm.childContextTypes = {
+    form: PropTypes.object,
+};
+
+ValidatorForm.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    instantValidate: PropTypes.bool,
+    children: PropTypes.node,
+    onError: PropTypes.func,
+    debounceTime: PropTypes.number,
+};
+
+ValidatorForm.defaultProps = {
+    onError: () => {},
+    debounceTime: 0,
+};
+
+export default ValidatorForm;
